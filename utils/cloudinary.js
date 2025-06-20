ee84e8e00357d3a118afa042e0e7aeefff58482e
Anton Volkov <antvolkov84@gmail.com>
2025-06-20 09:16:40 +0300

    cloudinary

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,17 +1,13 @@
-const crypto = require("crypto");
-
 exports.deleteImageFromCloudinary = async (req, res) => {
   const { publicId } = req.body;
-
   if (!publicId) {
     return res.status(400).json({ error: "publicId is required" });
   }
-
   try {
     const timestamp = Math.floor(Date.now() / 1000);
     const stringToSign = `public_id=${publicId}&timestamp=${timestamp}${process.env.EXPO_PUBLIC_APPLICATION_KEY_SECRET}`;
-    const signature = crypto.createHash("sha1").update(stringToSign).digest("hex");
-
+    const CryptoJS = require("crypto");
+    const signature = CryptoJS.createHash("sha1").update(stringToSign).digest("hex");
     const response = await fetch("https://api.cloudinary.com/v1_1/dmmixwibz/image/destroy", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
@@ -22,9 +18,7 @@ exports.deleteImageFromCloudinary = async (req, res) => {
         signature,
       }),
     });
-
     const result = await response.json();
-
     if (result.result === "ok") {
       return res.status(200).json({ success: true });
     } else {
