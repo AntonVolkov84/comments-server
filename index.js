8e6542a3380fe8816cdd4775005aa250c2e5d546
Anton Volkov <antvolkov84@gmail.com>
2025-06-20 09:08:07 +0300

    mistakes repair in path to file clodinary

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const http = require("http");
 const WebSocket = require("ws");
 const helmet = require("helmet");
-const { deleteImageFromCloudinary } = require("../utils/cloudinary");
+const { deleteImageFromCloudinary } = require("./utils/cloudinary");
 
 const app = express();
 const server = http.createServer(app);
