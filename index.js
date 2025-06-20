a8b04de32a2b0374808e36e1dfedc6bccc35bf82
Anton Volkov <antvolkov84@gmail.com>
2025-06-20 11:10:31 +0300

    integrated postgradeSQL

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const http = require("http");
 const WebSocket = require("ws");
 const helmet = require("helmet");
+const pool = require("./utils/database");
 const { deleteImageFromCloudinary } = require("./utils/cloudinary");
 
 const app = express();
@@ -14,6 +15,15 @@ app.use(helmet());
 app.use(express.json());
 
 app.delete("/delete-image", deleteImageFromCloudinary);
+app.get("/test-db", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json({ time: result.rows[0] });
+  } catch (err) {
+    console.error("DB error:", err.message);
+    res.status(500).json({ error: "Database error" });
+  }
+});
 
 require("./websocket/websocket")(wss);
 
