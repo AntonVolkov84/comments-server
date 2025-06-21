88488334842074476f40bda08d6661752293c540
Anton Volkov <antvolkov84@gmail.com>
2025-06-21 12:23:32 +0300

    credentials for websocket

--- websocket/websocket.js
+++ websocket/websocket.js
@@ -1,17 +1,25 @@
 module.exports = (wss) => {
   wss.on("connection", (ws) => {
     console.log("New WebSocket connection");
-
     ws.on("message", (message) => {
       console.log("Received:", message);
-
+      let parsedMessage;
+      try {
+        parsedMessage = JSON.parse(message);
+      } catch (e) {
+        console.log("Message is not JSON");
+        return;
+      }
+      const broadcastMessage = JSON.stringify({
+        type: "new_post",
+        data: parsedMessage,
+      });
       wss.clients.forEach((client) => {
         if (client.readyState === ws.OPEN) {
-          client.send(`Echo: ${message}`);
+          client.send(broadcastMessage);
         }
       });
     });
-
     ws.on("close", () => {
       console.log("WebSocket disconnected");
     });
